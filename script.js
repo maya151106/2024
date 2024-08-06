6bd4b4477692000c97d9b791082a8c3e49cf276d
maya151106 <110244649+maya151106@users.noreply.github.com>
2024-08-06 09:15:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
-                afirmacao: "afirmação"
-            },
-            {
-                texto: "Isso é maravilhoso!" ,
-                afirmacao: "afirmação" 
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
-            }
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-        ]
-    },
+const perguntas = [
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Homofobia são atos preconceituosos contra pesssoas que tem orientação sexual diferente da heterossexual.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-@@ -33,7 +32,7 @@ const perguntas = [
-        ]
-    },
-    {
-        pergunta: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-@@ -43,7 +42,6 @@ const perguntas = [
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Isso é assustador!",
                 afirmacao: "afirmação"
             }
-
         ]
     },
     {
-@@ -57,25 +55,24 @@ const perguntas = [
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "afirmação"
-            }
-
-
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "A homofobia se manifesta na sociedade atraves de discriminação, violência, negligência nos serviços de saúde e legislação discriminatória.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                texto: "próximo",
                 afirmacao: "afirmação"
-            },
+            }
+        ]
+    },
+    {
+        enunciado: "O respeito à diversidade de gênero é importante para garantir igualdade de direitos, reconhecera multiplicidade de experiências humanas, reduzir a discriminação e promover o bem estar.",
+        alternativas: [
             {
-                texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                texto: "próximo",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "a homofobia pode causar ansiedade, depressão, isolamento social, trauma psicológico, barreiras ao acesso de serviços de saúde e estresse crônico.",
+        alternativas: [
+            {
+                texto: "próximo",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Agora você está ciente sobre o que é a Homofobia e o que ela causa.",
+        alternativas: [
+            {
+                texto: "Fim.",
                 afirmacao: "afirmação"
             }
         ]
@@ -61,20 +56,38 @@
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
-@@ -91,8 +88,8 @@ function mostraPergunta() {
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-@@ -113,4 +110,4 @@ function mostraResultado() {
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
 
-mostraPergunta();
 mostraPergunta();
